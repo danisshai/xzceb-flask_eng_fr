dbffe3fc4b6093c0e9da4751d3964e4462d06288
danisshai <dsandoval.ai@gmail.com>
2023-03-03 18:26:05 -0500

    checking format with pylint

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,15 +1,14 @@
 import unittest
-from  translator import frenchToEnglish, englishToFrench
+from  translator import french_to_english, english_to_french
 class TestStringMethods(unittest.TestCase):
 
     def test_null(self):
-        self.assertEqual(frenchToEnglish(None),"")
-        self.assertEqual(englishToFrench(None),"")
+        self.assertEqual(french_to_english(None),"")
+        self.assertEqual(english_to_french(None),"")
 
     def test_translation(self):
-        self.assertEqual(frenchToEnglish("Bonjour"),"Hello")
-        self.assertEqual(englishToFrench("Hello"),"Bonjour")
-
+        self.assertEqual(french_to_english("Bonjour"),"Hello")
+        self.assertEqual(english_to_french("Hello"),"Bonjour")
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,20 +1,25 @@
+"""
+high level support for doing this and that.
+"""
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-authenticator = IAMAuthenticator('{apikey}')
+
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('{url}')
+language_translator.set_service_url(url)
+language_translator.set_disable_ssl_verification(True)
 
 
 def translator_instance(text, model):
@@ -26,13 +31,19 @@ def translator_instance(text, model):
     translation = language_translator.translate(
         text=text,
         model_id=model).get_result()
-    return(json.dumps(translation, indent=2, ensure_ascii=False))
+    return json.dumps(translation, indent=2, ensure_ascii=False)
 
-def englishToFrench(text):
+def english_to_french(text):
+    """
+    Translated the English text to French
+    """
     #write the code here
-    translator_instance(text, "en-fr")
-    return frenchText
+    french_text = translator_instance(text, "en-fr")
+    return french_text
 
-def englishToFrench(text):
-    translator_instance(text, "fr-en")
-    return englishText
+def french_to_english(text):
+    """
+    Translated the French text to English
+    """
+    english_text = translator_instance(text, "fr-en")
+    return english_text
